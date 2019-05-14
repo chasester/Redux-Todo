b08c6f0643872c6559094c16b9241754ac19ebb0
Chase Wex <clapingmanly@hotmail.com>
2019-05-14 19:08:11 -0400

    Created the store and assigned it based on the reducer

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,17 +3,26 @@ import './App.css';
 
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import controller from './reducer/index'
+import TodoList from './Component/TodoList';
+import TodoForm from './Component/TodoForm';
 
-const store = createStore(counter);
+const store = createStore(controller);
 
-function App() {
-  return (
-    <div className="App">
-    <Provider store={store} >
-      hello world
-    </div>
-    </Provider>
-  );
+class App extends React.Component 
+{
+  render(){
+    return (
+      <div className="App">
+      <Provider store={store} >
+        <TodoList />
+        <TodoForm />
+      </Provider>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
