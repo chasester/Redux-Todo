b599f8f59465ada9f6cb0519a679a88900bd4716
Chase Wex <clapingmanly@hotmail.com>
2019-05-14 19:57:29 -0400

    Updated todo list to be better for the formating css

--- todo/src/Component/TodoList.js
+++ todo/src/Component/TodoList.js
@@ -7,16 +7,16 @@ class TodoList extends React.Component
 {
     render()
     {
-        return (<div className="todo-list">
+        return (<ul className="todo-list">
             {
                 this.props.todos.map((x,i)=> 
-                    <div onClick={() => this.props.changeTodo(x.id)} className="todo" key={i}>
+                    <li onClick={() => this.props.changeTodo(x.id)} className="todo-item" key={i}>
                         {x.status ? <strike>{x.name}</strike> : <span>{x.name}</span>}
-                        <button onClick={() => this.props.removeTodo(x.id)}>x</button>
-                    </div> 
+                        <button onClick={() => this.props.removeTodo(x.id)} >remove</button>
+                    </li> 
                 )
             }
-        </div>);
+        </ul>);
     }
 }
 
